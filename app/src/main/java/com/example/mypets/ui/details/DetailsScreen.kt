ab769842950ea6144d66a806457b1f311d452088
jorgevalencia18 <jorgevalenciaabad18@gmail.com>
2023-04-13 10:31:16 +0200

    commit

--- app/src/main/java/com/example/mypets/ui/details/DetailsScreen.kt
+++ app/src/main/java/com/example/mypets/ui/details/DetailsScreen.kt
@@ -1,15 +1,12 @@
 package com.example.mypets.ui.details
 
 import android.util.Log
-import android.widget.Button
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
 import com.example.mypets.R
-import com.example.mypets.domain.model.Pet
 import com.example.mypets.ui.Header
 
 @Composable
